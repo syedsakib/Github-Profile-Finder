182143edf5fb7a71c2f32dd88ab938c48804034a
Syed Nazmus Shakib <sakibsyed01@gmail.com>
2019-09-06 01:00:53 +0600

    init

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+    constructor() {
+        this.client_id = '8d649961b1d9d468cdab';
+        this.client_secret = '3e0c39d8f1ca32d8b6ef63da55371ad587ae4b86';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
